995b444efb39300ab365f12e7b5a8187435fd3e4
Игорь Попов <igorpopov@MacBook-Pro-Igor.local>
2020-09-22 15:53:45 +0300

    work

--- Dota 2 Heroes Wiki/Dota 2 Heroes Wiki/HeroesList/SearchViewController.swift
+++ Dota 2 Heroes Wiki/Dota 2 Heroes Wiki/HeroesList/SearchViewController.swift
@@ -10,10 +10,45 @@ import UIKit
 
 class SearchViewController: UITableViewController {
     
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    let heroesNetworkManager = HeroesNetworkManager()
+    var heroesList = [Heroes]()
+    var heroes: Heroes?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        heroesNetworkManager.getHeroesList(result: {[ weak self] in
+            self?.heroesList = $0.localized_name
+        })
+        setupSearchBar()
         view.backgroundColor = .white
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
     
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return heroesList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        let hero = heroes[indexPath.row]
+        cell.textLabel?.text = "test"
+        cell.imageView?.image = #imageLiteral(resourceName: "search")
+        return cell
+    }
+    
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
 }
--- Dota 2 Heroes Wiki/Dota 2 Heroes Wiki/Services/HeroesNetworkManager.swift
+++ Dota 2 Heroes Wiki/Dota 2 Heroes Wiki/Services/HeroesNetworkManager.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 AirIgor. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct HeroesNetworkManager {
+    func getHeroesList(result: @escaping ((Heroes) -> Void)) {
+        let urlString = ApiSend.startUrl + ApiSend.heroKey
+        guard let url = URL(string: urlString) else { return }
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let data = data {
+                self.parseJSON(withData: data, result: result)
+            }
+        }
+        task.resume()
+    }
+    
+    func parseJSON(withData data: Data, result: @escaping ((Heroes) -> Void)) {
+        let decoder = JSONDecoder()
+        do {
+            let getHeroesListData = try decoder.decode(Heroes.self, from: data)
+            result(getHeroesListData)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+
