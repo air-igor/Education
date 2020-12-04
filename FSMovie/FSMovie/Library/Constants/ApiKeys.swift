948e4b0be968057c727408f797eea11602955e92
Игорь Попов <igorpopov@MacBook-Pro-Igor.local>
2020-12-05 01:01:58 +0300

    add person keys

--- FSMovie/FSMovie/Library/Constants/ApiKeys.swift
+++ FSMovie/FSMovie/Library/Constants/ApiKeys.swift
@@ -16,6 +16,7 @@ enum ApiKeys {
     static let globalKey = "990e9ae77435b145536b30e7ba158fca"
     static let paramUrl = "&language=en-US&"
     static let detailMovie = "/movie/"
+    static let detailPerson = "/person/"
     static let detailKey = "?api_key=990e9ae77435b145536b30e7ba158fca"
     static let imageStartUrl = "https://image.tmdb.org/t/p/original"
     static let homeVideoEnd = "&language=en-US&page=1"
