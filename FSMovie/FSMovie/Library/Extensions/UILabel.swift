1fe916869a5b29cb3c8001b893910c9c5c948154
Игорь Попов <igorpopov@MacBook-Pro-Igor.local>
2020-12-05 01:03:31 +0300

    add code for person birthday

--- FSMovie/FSMovie/Library/Extensions/UILabel.swift
+++ FSMovie/FSMovie/Library/Extensions/UILabel.swift
@@ -93,4 +93,46 @@ extension UILabel {
                self.text = "Unknow"
            }
        }
+    
+    func setCorrectlyDate(_ personInfo: Person) {
+        guard let personBirthday = personInfo.birthday else { return }
+        let birthdayPerson = personBirthday.split(separator: "-")
+        if !birthdayPerson.isEmpty {
+            let year = birthdayPerson[0]
+            let day = birthdayPerson[2]
+            var month: Months = .undefined
+            switch birthdayPerson[1] {
+            case "01":
+                month = .january
+            case "02":
+                month = .february
+            case "03":
+                month = .march
+            case "04":
+                month = .april
+            case "05":
+                month = .may
+            case "06":
+                month = .june
+            case "07":
+                month = .july
+            case "08":
+                month = .august
+            case "09":
+                month = .september
+            case "10":
+                month = .october
+            case "11":
+                month = .november
+            case "12":
+                month = .december
+            default:
+                month = .undefined
+            }
+            self.text = "\(month.rawValue) \(day), \(year) "
+        }
+        else {
+            self.text = "Unknow"
+        }
+    }
 }
