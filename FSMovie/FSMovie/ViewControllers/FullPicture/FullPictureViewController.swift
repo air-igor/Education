e1e301a728773faacc6a888c36e010800479b95a
Игорь Попов <igorpopov@MacBook-Pro-Igor.local>
2020-12-05 01:47:48 +0300

    create fullPictureController

---
+++ FSMovie/FSMovie/ViewControllers/FullPicture/FullPictureViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FullPictureViewController.swift
+//  FSMovie
+//
+//  Created by Игорь Попов on 05.12.2020.
+//  Copyright © 2020 AirIgor. All rights reserved.
+//
+
+import UIKit
+
+class FullPictureViewController: UIViewController {
+    
+    var fullPicture: String?
+    
+    @IBOutlet weak var fullPictureImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setFullImage()
+    }
+    
+    func setFullImage() {
+        let imageUrl = ApiKeys.startImgUrl + fullPicture!
+        fullPictureImage.downloaded(from: imageUrl)
+        print(imageUrl)
+    }
+    
+}
