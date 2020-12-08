a515229e58a9b62aaef915eb18c05cb359c7981e
Игорь Попов <igorpopov@MacBook-Pro-Igor.local>
2020-12-08 21:32:15 +0300

    create personMovies model

--- FSMovie/FSMovie/Models/Entity/CastPersonMovies.swift
+++ FSMovie/FSMovie/Models/Entity/CastPersonMovies.swift
@@ -1,5 +1,5 @@
 //
-//  PersonMovies.swift
+//  CastPersonMovies.swift
 //  FSMovie
 //
 //  Created by Игорь Попов on 08.12.2020.
@@ -11,19 +11,12 @@ import Foundation
 
 struct CastPersonMovies: Decodable {
     let cast: [PersonCastMovies]
-    let crew: [PersonCrewMovies]
 }
 
 struct PersonCastMovies: Decodable {
     let id: Int?
-    let backdropPath: String?
+    let posterPath: String?
     let releaseDate: String?
     let title: String?
-}
-
-struct PersonCrewMovies: Decodable {
-    let id: Int?
-    let backdropPath: String?
-    let releaseDate: String?
-    let title: String?
+    let voteAverage: Double?
 }
