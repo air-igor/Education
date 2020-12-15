e077a9990f8d26039aab3aa4ac4164946ce21829
Игорь Попов <igorpopov@MacBook-Pro-Igor.local>
2020-12-15 20:52:42 +0300

    update code and add optimization for detailed window

---
+++ FSMovie/FSMovie/Library/Extensions/UICollectionView+RegisterCell.swift
@@ -0,0 +1,9 @@
+//
+//  UICollectionView+RegisterCell.swift
+//  FSMovie
+//
+//  Created by Игорь Попов on 12.12.2020.
+//  Copyright © 2020 AirIgor. All rights reserved.
+//
+
+import Foundation
---
+++ FSMovie/FSMovie/Library/Extensions/UITableView+RegisterCell.swift
@@ -0,0 +1,24 @@
+//
+//  UITableView.swift
+//  FSMovie
+//
+//  Created by Игорь Попов on 12.12.2020.
+//  Copyright © 2020 AirIgor. All rights reserved.
+//
+
+import UIKit
+
+
+extension UITableView {
+    func registerCell(type: UITableViewCell.Type, indentifier: String? = nil) {
+        let cellId = String(describing: type)
+        register(UINib(nibName: cellId, bundle: nil), forCellReuseIdentifier: indentifier ?? cellId)
+    }
+    
+}
+
+extension UITableViewCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
